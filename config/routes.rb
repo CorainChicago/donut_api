728262a62c631941f6988630584cf13802166e80
CorainChicago <chaysmagan@gmail.com>
2017-05-06 09:45:16 -0500

    getting failure errors when attempting to login, need to setup emailing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   #devise_for :users
-  #mount_devise_token_auth_for 'User', at: 'auth'
-  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+  mount_devise_token_auth_for 'User', at: 'auth'
+  get 'user/members_only', to: 'user#members_only'
+  #devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
-require 'rails_helper'
+# require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+
+  describe "sessions" do
+    before do
+      @user = FactoryGirl.create(:user)
+      @api_key = FactoryGirl.create(:api_key)
+    end
+
+    it "is authenticated with a token" do
+      put "/api/v1/users/#{@user.id}?user_email=#{@user.email}&auth_token=#{@user.authentication_token}", {user: {name: "New Name"}}, { "HTTP_AUTHORIZATION"=>"Token token=\"#{@api_key.access_token}\"" }
+      response.status.should be(201)
+    end
+  end
 end
